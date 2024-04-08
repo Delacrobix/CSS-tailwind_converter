b494a7f69c07ef576fa1a32acded1cb3c4cc4dbb
Jeffrey <jeffrey.rengifom@gmail.com>
2024-04-07 19:56:21 -0500

    title added

--- frontend/src/pages/principal.tsx
+++ frontend/src/pages/principal.tsx
@@ -4,7 +4,8 @@ import InputForm from "../components/inputForm";
 
 export default function Principal() {
   return (
-    <div className='h-[100vh] w-full flex justify-center items-center'>
+    <div className='flex flex-col justify-center items-center h-[100vh] w-full '>
+      <h1 className='text-4xl font-bold'>Tailwind CSS Converter</h1>
       <InputForm />
     </div>
   );
