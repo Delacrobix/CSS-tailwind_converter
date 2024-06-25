d135f7568254029cef697a6982d666b83bcc13fb
Jeffrey <jeffrey.rengifom@gmail.com>
2024-06-24 19:40:30 -0500

    adding docs to controllers

--- backend/src/cohere-api/cohere-api.controller.ts
+++ backend/src/cohere-api/cohere-api.controller.ts
@@ -10,11 +10,21 @@ import {
 export class CohereAIApiController {
   constructor(private readonly service: CohereAIApiService) {}
 
+  /**
+   * Endpoint for health check.
+   * @returns 'OK' if the server is running.
+   */
   @Get('health')
   async healthCheck() {
     return 'OK';
   }
 
+  /**
+   * Endpoint for converting CSS to Tailwind CSS.
+   * @param data - The input data containing the CSS code.
+   * @returns The converted Tailwind CSS code.
+   * @throws Error if an error occurs during the conversion process.
+   */
   @Post('css-to-tailwind')
   async cssToTailwind(
     @Body(new ValidationPipe({ transform: true }))
@@ -29,6 +39,12 @@ export class CohereAIApiController {
     }
   }
 
+  /**
+   * Endpoint for converting Tailwind CSS to CSS.
+   * @param data - The input data containing the Tailwind CSS code.
+   * @returns The converted CSS code.
+   * @throws Error if an error occurs during the conversion process.
+   */
   @Post('tailwind-to-css')
   async tailwindToCss(
     @Body(new ValidationPipe({ transform: true }))
